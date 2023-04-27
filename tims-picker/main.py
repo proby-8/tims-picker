b3fd202736887c2d48ce32254bb82d71e2814ba9
Nathan Probert <nathanprobert@rogers.com>
2023-04-27 13:42:59 -0400

    added new main file

---
+++ tims-picker/main.py
@@ -0,0 +1,60 @@
+import time
+from datetime import datetime
+import scrapingFunctions
+import os
+import guessMaker
+
+path = "lib\\"
+try:
+    os.mkdir(path)
+except:
+    pass
+
+print("\nPlease select a function:")
+print("1: Make a guess using a predefined formula")
+print("2: Make a guess using an AI formulated calculation")
+print("3: Add more data to the csv data files")
+choice = int(input())
+
+if (choice == 1):
+    guessMaker.main()
+elif(choice == 2):
+    print("No")
+elif (choice == 3):
+    daysGoingUp=True
+    print("Would you like to add recent games, or continue adding old games (o/n)? ", end="")
+    if (input() == 'o'):
+        daysGoingUp = False
+
+    print("How many days would you like to add? ", end="")
+    daysToLookAt = input()
+
+    try:
+        if (daysGoingUp):
+            inputFile = (path + 'train_test1.csv')
+            f1 = open(inputFile, "r")
+            first_line = f1.readlines()[1]
+            f1.close()
+            date = first_line[:10]
+            date = scrapingFunctions.addDays(date)
+        else:
+            inputFile = (path + 'train_test1.csv')
+            f1 = open(inputFile, "r")
+            last_line = f1.readlines()[-1]
+            f1.close()
+            date = last_line[:10]
+            date = scrapingFunctions.subtractDays(date)
+    except:
+        # file DNE
+        date = datetime.today().strftime('%Y-%m-%d')
+        if (daysGoingUp == False):
+            date = scrapingFunctions.subtractDays(date)
+
+    print(f"Beginning at {date}")
+    start_time = time.time()
+
+    # path is path to file location
+    # date is date to start at
+    scrapingFunctions.getStats(path, date, daysToLookAt, daysGoingUp)
+
+    scrapingFunctions.printTime(time.time() - start_time)
